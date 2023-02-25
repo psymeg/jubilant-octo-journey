ccf2b40eff3fdd991361377115b7e4006f1c2bf4
psymeg <terryteo@gmail.com>
2023-02-26 11:34:38 +1300

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# HarvardX PH125.9x Data Science: Capstone
+
+Simon Gibson 
+
+## Movielens Project -  Machine Learning Submission
+
+### Introduction
+
+For the 9th Course in the HarvardX Data Science course we have been asked to build a movie recommendation system using the MovieLens dataset.
+
+The movie recommendation system will predict movie ratings.
+
+The Movielens data package can be found at [the MovieLens homepage](https://movielens.org/). 
+
+MovieLens is a project run by GroupLens - a research lab run at the University of Minnesota in North America. MovieLens is a non-commercial collection of movie data and the main set of data contains over 20 million ratings for over 27,000 movies. In this project we are using the [10M dataset](https://grouplens.org/datasets/movielens/10m/).
+
+Note: code in this package is written on Linux. If running on windows you may need to change the line endings. This code has not been tested on Windows.
+
+### Methods
+
+#### Processes and techniques used
+
+#### Data cleaning
+
+#### Data Exploration and Visualisation
+
+#### Modelling Approach
+
+### Results
+
+### Conclusion
+
+#### Limitations
+
+#### Future work
+
+### References
+
+1. http://rafalab.dfci.harvard.edu/dsbook/large-datasets.html#recommendation-systems
+
+2. https://learning.edx.org/course/course-v1:HarvardX+PH125.8x+3T2022/block-v1:HarvardX+PH125.8x+3T2022+type@sequential+block@7e7727ce543b4ed6ae6338626862eada/block-v1:HarvardX+PH125.8x+3T2022+type@vertical+block@df3d8a86b43f4247a4dd42bcabb1a663
+
---
+++ set_creation.r
@@ -0,0 +1,129 @@
+##########################################################
+# Create edx and final_holdout_test sets 
+##########################################################
+
+# Note: this process could take a couple of minutes
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+
+library(tidyverse)
+library(caret)
+
+# MovieLens 10M dataset:
+# https://grouplens.org/datasets/movielens/10m/
+# http://files.grouplens.org/datasets/movielens/ml-10m.zip
+
+options(timeout = 120)
+
+dl <- "ml-10M100K.zip"
+if(!file.exists(dl))
+  download.file("https://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+
+ratings_file <- "ml-10M100K/ratings.dat"
+if(!file.exists(ratings_file))
+  unzip(dl, ratings_file)
+
+movies_file <- "ml-10M100K/movies.dat"
+if(!file.exists(movies_file))
+  unzip(dl, movies_file)
+
+ratings <- as.data.frame(str_split(read_lines(ratings_file), fixed("::"), simplify = TRUE),
+                         stringsAsFactors = FALSE)
+colnames(ratings) <- c("userId", "movieId", "rating", "timestamp")
+ratings <- ratings %>%
+  mutate(userId = as.integer(userId),
+         movieId = as.integer(movieId),
+         rating = as.numeric(rating),
+         timestamp = as.integer(timestamp))
+
+movies <- as.data.frame(str_split(read_lines(movies_file), fixed("::"), simplify = TRUE),
+                        stringsAsFactors = FALSE)
+colnames(movies) <- c("movieId", "title", "genres")
+movies <- movies %>%
+  mutate(movieId = as.integer(movieId))
+
+movielens <- left_join(ratings, movies, by = "movieId")
+
+# Final hold-out test set will be 10% of MovieLens data
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+# set.seed(1) # if using R 3.5 or earlier
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+edx <- movielens[-test_index,]
+temp <- movielens[test_index,]
+
+# Make sure userId and movieId in final hold-out test set are also in edx set
+final_holdout_test <- temp %>% 
+  semi_join(edx, by = "movieId") %>%
+  semi_join(edx, by = "userId")
+
+# Add rows removed from final hold-out test set back into edx set
+removed <- anti_join(temp, final_holdout_test)
+edx <- rbind(edx, removed)
+
+rm(dl, ratings, movies, test_index, temp, movielens, removed)
+
+##########################################
+# combine with 8.6.2 recommendation code #
+##########################################
+
+head(edx)
+ncol(edx)
+nrow(edx)
+
+# code from harvadx course, see README.md Reference #2
+# naive rmse 
+
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
+
+mu_hat <- mean(edx$rating)
+mu_hat
+
+naive_rmse <- RMSE(edx$rating, mu_hat)
+naive_rmse
+# naive_rmse == 1.060331
+
+rmse_results <- tibble(method = "Just the average", RMSE = naive_rmse)
+
+# this errors as it is too large
+#fit <- lm(rating ~ as.factor(userId), data = edx)
+
+mu <- mean(train_set$rating) 
+movie_avgs <- train_set %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = mean(rating - mu))
+
+predicted_ratings <- mu + edx %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  pull(b_i)
+
+model_1_rmse <- RMSE(predicted_ratings, edx$rating)
+model_1_rmse
+rmse_results <- rmse_results %>% add_row(method = "Group By MovieID", RMSE = model_1_rmse)
+
+
+# check if the same user effect (many ratings) is present in this codeset as it was in unit 8
+edx %>% 
+  group_by(userId) %>% 
+  summarize(b_u = mean(rating)) %>% 
+  filter(n()>=100) %>%
+  ggplot(aes(b_u)) + 
+  geom_histogram(bins = 30, color = "orange")
+
+#compute an approximation by computing mu and bi and estimating bu as the average of : 
+user_avgs <- edx %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  group_by(userId) %>%
+  summarize(b_u = mean(rating - mu - b_i))
+
+predicted_ratings <- edx %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  left_join(user_avgs, by='userId') %>%
+  mutate(pred = mu + b_i + b_u) %>%
+  pull(pred)
+
+model_2_rmse <- RMSE(predicted_ratings, edx$rating)
+
+rmse_results <- rmse_results %>% add_row(method = "Group By userID", RMSE = model_2_rmse)
